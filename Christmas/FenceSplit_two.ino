465540eae14d9fa9319468bf1c435efe3297f2d5
Erik <esvanoe@gmail.com>
2022-11-25 21:11:26 -0700

    Adding new Xmas Fence code

--- Christmas/FenceSplit_two.ino
+++ Christmas/FenceSplit_two.ino
@@ -1,22 +1,24 @@
-/* ======================= includes ================================= */
+/* 
+This is a copy of my first fence code. THIS ONE IS FOR PLAYING/TESTING
 
-#include "Particle.h"
-#include "neopixel.h"
-
-/* ======================= functions =============================== */
-
-void colorFade(uint8_t reps, uint16_t wait);
-void colorSplit(uint16_t wait);
-void colorOff(uint16_t wait);
-
-/* ======================= extra-examples.cpp ======================== */
+[TO-DO]
+Variablize the lengths and halves
+Add cool functions
+*/ 
 
 SYSTEM_MODE(AUTOMATIC);
 SYSTEM_THREAD(ENABLED);
+/* ======================= includes ================================= */
+#include "Particle.h"
+#include "neopixel.h"
+/* ======================= functions =============================== */
+void colorFade(uint8_t reps, uint16_t wait);
+void colorSplit(uint16_t wait);
+void colorOff(uint16_t wait, uint16_t down, uint16_t up);
 
 // IMPORTANT: Set pixel COUNT, PIN and TYPE
-#define PIXEL_COUNT 300
-#define PIXEL_PIN D5
+#define PIXEL_COUNT 350
+#define PIXEL_PIN D6
 #define PIXEL_TYPE WS2811
 
 Adafruit_NeoPixel strip(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
@@ -28,11 +30,11 @@ void setup() {
 }
 
 void loop() {
-  strip.setPixelColor(0, strip.Color(0, 0, 0));
-  strip.show();
-  colorFade(3, 30); //reps, wait
-  colorOff(200);
-  colorSplit(50);   // wait
+  colorFade(2, 10); //reps, wait
+  colorOff(100); // wait
+  colorSplit(100, 175, 176);   // wait, down, up
+  colorOff(100); // wait
+  colorSplit(100, 175, 176);   // wait, down, up
 }
 
 void colorFade(uint8_t reps, uint16_t wait) {
@@ -69,23 +71,29 @@ void colorFade(uint8_t reps, uint16_t wait) {
 }
 
 // Fill the dots two after two with a color, wait (ms) after each one
-void colorSplit uint16_t wait) {
-  for(uint16_t i=PIXEL_COUNT/2+1; i<strip.numPixels(); i+=4) {
-    for(uint16_t f=PIXEL_COUNT/2; i>0; i-=4) {
-        strip.setPixelColor(i, strip.Color(0, 0, 250));
-        strip.setPixelColor(i+1, strip.Color(0, 0, 250));
-        strip.setPixelColor(f, strip.Color(0, 0, 250));
-        strip.setPixelColor(f-1, strip.Color(0, 0, 250));
-        strip.show();
-        delay(wait);
-        strip.setPixelColor(i, strip.Color(250, 250, 250));
-        strip.setPixelColor(i+1, strip.Color(250, 250, 250));
-        strip.setPixelColor(f, strip.Color(250, 250, 250));
-        strip.setPixelColor(f-1, strip.Color(250, 250, 250));
-        strip.show();
-        delay(wait);
-        }
-    } 
+void colorSplit(uint16_t wait, uint16_t down, uint16_t up) {
+  while(down>=0 && up<=350) {
+    strip.setPixelColor(up, strip.Color(0, 0, 250));
+    strip.setPixelColor(up+1, strip.Color(0, 0, 250));
+    strip.setPixelColor(down, strip.Color(0, 0, 250));
+    strip.setPixelColor(down-1, strip.Color(0, 0, 250));
+    strip.show();
+    delay(wait);
+    up+=4;
+    down-=4;
+  }
+  down=175;
+  up=176;
+  while(down>=0 && up<=350) {
+    strip.setPixelColor(up+2, strip.Color(250, 250, 250));
+    strip.setPixelColor(up+3, strip.Color(250, 250, 250));
+    strip.setPixelColor(down-2, strip.Color(250, 250, 250));
+    strip.setPixelColor(down-3, strip.Color(250, 0, 250));
+    strip.show();
+    delay(wait);
+    up+=4;
+    down-=4;
+  }
 }
 
 void colorOff(uint16_t wait) {
