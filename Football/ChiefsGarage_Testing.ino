33cd2e6cbd94a3bc2efa66ba169e91a5cc925c47
Erik <esvanoe@gmail.com>
2025-04-24 14:16:39 -0600

    update testing code

--- Football/ChiefsGarage_Testing.ino
+++ Football/ChiefsGarage_Testing.ino
@@ -9,18 +9,6 @@ void colorFlash(uint8_t reps, uint16_t wait);
 void colorWipe(uint16_t wait);
 void colorOff(uint16_t wait);
 void twinkleWhite(uint8_t wait, uint32_t color1, uint32_t color2);
-long randN; 
-long randN1;
-long randN2;
-long randN3; 
-long randN4;
-long randN5;
-long randN6; 
-long randN7;
-long randN8;
-long randN9; 
-long randN10;
-long randN11;
 
 /* ======================= extra-examples.cpp ======================== */
 
@@ -121,49 +109,51 @@ void colorFlash(uint16_t wait) {
    }
 
 void twinkleWhite(uint8_t wait, uint32_t color1, uint32_t color2) {
+  #define NUM_RANDOMS 12
+  long randNumbers[NUM_RANDOMS];
+  
   for(uint8_t f=0; f<201; f++) {
-    randN = random(0, 160);
-    randN1 = random(161, 320);
-    randN2 = random(0, 160);
-    randN3 = random(161, 320);
-    randN4 = random(0, 160);
-    randN5 = random(161, 320);
-    randN6 = random(0, 160);
-    randN7 = random(161, 320);
-    randN8 = random(0, 160);
-    randN9 = random(161, 320);
-    randN10 = random(0, 160);
-    randN11 = random(161, 320);
-    strip.setPixelColor(randN, color1);
-    strip.setPixelColor(randN1, color1);
-    strip.setPixelColor(randN2, color2);
-    strip.setPixelColor(randN3, color2);
-    strip.setPixelColor(randN4, color1);
-    strip.setPixelColor(randN5, color1);
-    strip.show();
-    delay(wait);
-    strip.setPixelColor(randN6, color1);
-    strip.setPixelColor(randN7, color1);
-    strip.setPixelColor(randN8, color2);
-    strip.setPixelColor(randN9, color2);
-    strip.setPixelColor(randN10, color1);
-    strip.setPixelColor(randN11, color1);
-    strip.setPixelColor(randN, strip.Color(0, 0, 0));
-    strip.setPixelColor(randN1, strip.Color(0, 0, 0));
-    strip.setPixelColor(randN2, strip.Color(0, 0, 0));
-    strip.setPixelColor(randN3, strip.Color(0, 0, 0));
-    strip.setPixelColor(randN4, strip.Color(0, 0, 0));
-    strip.setPixelColor(randN5, strip.Color(0, 0, 0));
-    strip.show();
-    delay(wait);
-    strip.setPixelColor(randN6, strip.Color(0, 0, 0));
-    strip.setPixelColor(randN7, strip.Color(0, 0, 0));
-    strip.setPixelColor(randN8, strip.Color(0, 0, 0));
-    strip.setPixelColor(randN9, strip.Color(0, 0, 0));
-    strip.setPixelColor(randN10, strip.Color(0, 0, 0));
-    strip.setPixelColor(randN11, strip.Color(0, 0, 0));
-    strip.show();    
+    // Generate all random numbers at once
+    for(int i = 0; i < NUM_RANDOMS; i++) {
+      // Alternate between first half (0-160) and second half (161-320)
+      randNumbers[i] = random(i % 2 == 0 ? 0 : 161, i % 2 == 0 ? 160 : 320);
     }
+    
+    // First set of pixels (turn on)
+    strip.setPixelColor(randNumbers[0], color1);
+    strip.setPixelColor(randNumbers[1], color1);
+    strip.setPixelColor(randNumbers[2], color2);
+    strip.setPixelColor(randNumbers[3], color2);
+    strip.setPixelColor(randNumbers[4], color1);
+    strip.setPixelColor(randNumbers[5], color1);
+    strip.show();
+    delay(wait);
+    
+    // Second set of pixels (turn on) and turn off first set
+    strip.setPixelColor(randNumbers[6], color1);
+    strip.setPixelColor(randNumbers[7], color1);
+    strip.setPixelColor(randNumbers[8], color2);
+    strip.setPixelColor(randNumbers[9], color2);
+    strip.setPixelColor(randNumbers[10], color1);
+    strip.setPixelColor(randNumbers[11], color1);
+    strip.setPixelColor(randNumbers[0], strip.Color(0, 0, 0));
+    strip.setPixelColor(randNumbers[1], strip.Color(0, 0, 0));
+    strip.setPixelColor(randNumbers[2], strip.Color(0, 0, 0));
+    strip.setPixelColor(randNumbers[3], strip.Color(0, 0, 0));
+    strip.setPixelColor(randNumbers[4], strip.Color(0, 0, 0));
+    strip.setPixelColor(randNumbers[5], strip.Color(0, 0, 0));
+    strip.show();
+    delay(wait);
+    
+    // Turn off second set
+    strip.setPixelColor(randNumbers[6], strip.Color(0, 0, 0));
+    strip.setPixelColor(randNumbers[7], strip.Color(0, 0, 0));
+    strip.setPixelColor(randNumbers[8], strip.Color(0, 0, 0));
+    strip.setPixelColor(randNumbers[9], strip.Color(0, 0, 0));
+    strip.setPixelColor(randNumbers[10], strip.Color(0, 0, 0));
+    strip.setPixelColor(randNumbers[11], strip.Color(0, 0, 0));
+    strip.show();    
+  }
 }
 
 void colorOff(uint16_t wait) {
